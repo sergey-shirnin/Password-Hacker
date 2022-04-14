27ec4b6faf2b92d8da08ae08c0ff2a2121a91a40
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-04-14 20:05:10 +1100

    Stage 3

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import socket
 import sys
-from string import ascii_lowercase, digits
 from itertools import product
-
+import json
 
 args = sys.argv
 host = (args[1], int(args[2]))
 buffer = 1024
 
-char_base = ''.join((ascii_lowercase, digits))
-pw_gen = (comb for i in range(1, len(char_base) + 1) for comb in product(char_base, repeat=i))
-success_msg = 'Connection success!'; response = ''
+fail_login = 'Wrong login!'
+success_msg = 'Connection success!'; response = ''; found = False
 
 with socket.socket() as my_socket:
     my_socket.connect(host)
-    while response != success_msg:
-        pw = ''.join(next(pw_gen))
-        my_socket.send(pw.encode())
-        response = my_socket.recv(buffer).decode()
-    print(pw)
+
+    with open('logins.txt', 'r') as logs:
+        dummy_gen = (line.strip() for line in logs.readlines())
+        while response == fail_login:
+            guess = next(dummy_gen)
+            context = {
+                'admin': guess,
+                'password': ' '
+            }
+            my_socket.send(json.dumps(context))
+            response = my_socket.recv(buffer).decode()
+
+    with open('passwords.txt', 'r') as pws:
+        dummy_gen = (line.strip() for line in pws.readlines())
+        for dummy in dummy_gen:
+            opt_gen = product(*((char.lower(), char.upper()) if not char.isnumeric()
+                                else char for char in dummy))
+            for opt in opt_gen:
+                opt = ''.join(opt)
+                my_socket.send(opt.encode())
+                response = my_socket.recv(buffer).decode()
+                if response == success_msg:
+                    found = True
+                    break
+            if found:
+                break
+    print(opt)
